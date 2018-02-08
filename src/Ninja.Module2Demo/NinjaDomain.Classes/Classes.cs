41521f99c9d704e09eb609a0f5022400dbe9fa1a
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2018-02-08 21:57:01 +0200

    Add initial solution

---
+++ src/Ninja.Module2Demo/NinjaDomain.Classes/Classes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaDomain.Classes
+{
+    public class Ninja
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool ServedInOniwaban { get; set; }
+        public Clan Clan { get; set; }
+        public int ClanId { get; set; }
+        public List<NinjaEquipment> EquipmentOwned { get; set; }
+    }
+
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+    }
+
+    public class NinjaEquipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EquipmentType Type { get; set; }
+        [Required]
+        public Ninja Ninja { get; set; }  
+    }
+}
+
---
+++ src/Ninja.Module2Demo/NinjaDomain.DataModel/Migrations/201802081922450_Initial.cs
@@ -0,0 +1,58 @@
+namespace NinjaDomain.DataModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clans",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ClanName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.NinjaEquipments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Type = c.Int(nullable: false),
+                        Ninja_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Ninjas", t => t.Ninja_Id, cascadeDelete: true)
+                .Index(t => t.Ninja_Id);
+            
+            CreateTable(
+                "dbo.Ninjas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        ServedInOniwaban = c.Boolean(nullable: false),
+                        ClanId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Clans", t => t.ClanId, cascadeDelete: true)
+                .Index(t => t.ClanId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.NinjaEquipments", "Ninja_Id", "dbo.Ninjas");
+            DropForeignKey("dbo.Ninjas", "ClanId", "dbo.Clans");
+            DropIndex("dbo.Ninjas", new[] { "ClanId" });
+            DropIndex("dbo.NinjaEquipments", new[] { "Ninja_Id" });
+            DropTable("dbo.Ninjas");
+            DropTable("dbo.NinjaEquipments");
+            DropTable("dbo.Clans");
+        }
+    }
+}
